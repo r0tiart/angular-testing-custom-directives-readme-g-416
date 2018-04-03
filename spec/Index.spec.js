32a483da320d5f958903630a94f601c04e3e0319
r0tiart <antley.chu@gmail.com>
2018-04-03 12:08:33 -0400

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -5,13 +5,13 @@ describe('Directive Test', function() {
 	var count = element(by.css('.counter__count'));
 
 	it('should have an initial 0 count', function () {
-		expect(count.getInnerHtml()).toEqual('Current count: 0');
+		expect(count.getText()).toEqual('Current count: 0');
 	});
 
 	it('should increment when we click on it', function () {
 		counter.click();
 
-		expect(count.getInnerHtml()).toEqual('Current count: 1');
+		expect(count.getText()).toEqual('Current count: 1');
 	});
 
 });
